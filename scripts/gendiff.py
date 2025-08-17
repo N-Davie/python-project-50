ef4f240b1e62a91c96ee6445810f534a4c9ddabe
Davie <anytka-97@mail.ru>
2025-08-17 14:11:13 +0500

    Добавлены файлы и их чтение

--- scripts/gendiff.py
+++ scripts/gendiff.py
@@ -1,4 +1,9 @@
 import argparse
+import json
+
+def read_json(file_path):
+    with open(file_path) as file:
+        return json.load(file)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -13,6 +18,12 @@ def main():
         help='set format of output'
     )
     args = parser.parse_args()
+
+    data1 = json.load(open(args.first_file))
+    data2 = json.load(open(args.second_file))
+
+    print("Данные из первого файла:", data1)
+    print("Данные из второго файла:", data2)
 
 if __name__ == "__main__":
     main()
