46f1234e6af3a8be2273be3a1d619ce61ccaf5ef
Davie <anytka-97@mail.ru>
2025-08-25 23:17:44 +0500

    Add tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,11 +1,51 @@
 import pytest
 from gendiff.gendiff import generate_diff
 
-json_full = 'tests/fixtures/file1.json'
-json_some_equall = 'tests/fixtures/file2.json'
-json_full_anotherkeys = 'tests/fixtures/file3.json'
-json_empty = 'tests/fixtures/file4.json'
+json_full = 'tests/fixtures/file1_test.json'
+json_some_equall = 'tests/fixtures/file2_test.json'
+json_full_anotherkeys = 'tests/fixtures/file3_test.json'
+json_empty = 'tests/fixtures/file4_test.json'
 
 
-def generate_diff(json_full, json_some_equall):
-   assert "{\n" + "\n".join(result_lines) + "\n}"
+def test_generate_diff_with_pare():
+    result = generate_diff(json_full, json_some_equall)
+    expected = """{
+    age: 17
+  - best_friend: "Ron"
+  + best_friend: "books"
+    faculty: "Griffindor"
+  + hobby: "help house elfs"
+  - name: "harry"
+  + name: "hermione"
+    school: "hogwards"
+}"""
+    assert result == expected
+
+
+def test_generate_diff_without_pare():
+    result = generate_diff(json_full, json_full_anotherkeys)
+    expected = """{
+  - age: 17
+  - best_friend: "Ron"
+  + best_friend: "books"
+  + character: "cunning"
+  + dinasty: 1000
+  - faculty: "Griffindor"
+  + family: "Malfoy"
+  - name: "harry"
+  + passion: "dark magic"
+  - school: "hogwards"
+}"""
+    assert result == expected
+
+
+def test_generate_diff_with_empty():
+    result = generate_diff(json_full, json_empty)
+    expected = """{
+  - age: 17
+  - best_friend: "Ron"
+  - faculty: "Griffindor"
+  - name: "harry"
+  - school: "hogwards"
+}"""
+    assert result == expected
